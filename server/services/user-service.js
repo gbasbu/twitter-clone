4d57a0eb04316eeaa72694b4fde47ab0bd682fb7
gbasbu <gorkembasbu@gmail.com>
2021-07-01 00:00:42 +0300

    Add New Service Items

--- server/services/user-service.js
+++ server/services/user-service.js
@@ -8,6 +8,10 @@ class UserService extends BaseService {
         return this.model.findOne({ email: email })
     }
 
+    async findByUsername(username){
+        return this.model.findOne({ username: username })
+    }
+
     async createPass(user, password){
         user.password = password
         await user.save()
@@ -16,6 +20,16 @@ class UserService extends BaseService {
     async changeProfilePicture(user, url, id){
         user.profile_img_url = url
         user.cloudinaryId = id
+        await user.save()
+    }
+
+    async updateVerifyCode(user, code){
+        user.verify_code = code
+        await user.save()
+    }
+
+    async unloadVerifyCode(user){
+        user.verify_code = ''
         await user.save()
     }
 }
